eed0d0764734b34d78d5df35d13a296f5b63d7da
Ahmad Zaki Sultani <ahmadzs@evertz.com>
2023-09-26 16:35:23 -0400

    Added the entire code inside a function

--- expenses.py
+++ expenses.py
@@ -16,15 +16,20 @@ Dining 75.90
 
 with open('expenses.txt', 'w') as file:
     file.write(content_expenses)
+def total_expense(filename):
 
-total_cost = 0
+    total_cost = 0
 
-with open('expenses.txt', 'r') as f:
-    for line in f:
-        name, expense = line.split()
-        total_cost = total_cost + float(expense)
-print(f'total_cost = {total_cost}')
+    with open('expenses.txt', 'r') as f:
+        for line in f:
+            name, expense = line.split()
+            total_cost = total_cost + float(expense)
+    print(f'total_cost = {total_cost}')
 
 
-with open('expenses.txt','a') as f:
-    f.write(f'Total Cost = {str(total_cost)}')
+    with open('expenses.txt','a') as f:
+        f.write(f'Total Cost = {str(total_cost)}')
+
+if __name__ == '__main__':
+    cost = total_expense('expenses.txt')
+    
